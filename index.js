c253c35933226f8afcd91455210865b7493eac33
Raj Pandya <rajpandya737@gmail.com>
2023-09-01 16:43:44 -0400

    reset design

--- index.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-  let modeToggle = document.getElementById("btn");
-  modeToggle.onclick = function () {
-    document.body.classList.toggle("dark-theme");
-    document.body.style.transition = "0.5s";
-  };
-});
